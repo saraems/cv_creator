53ef10c865a65e1a006253aee360fdd0e354e6f4
Sara Waclawiec <sara.ems.94@gmail.com>
2019-02-15 12:01:57 +0100

    Updates

---
+++ precache-manifest.5278e87cf7563f9c772113ef7a85bf4d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e36c0aaaeb10bf4d919a",
+    "url": "/cv_creator/static/css/main.6d5ea7b1.chunk.css"
+  },
+  {
+    "revision": "e36c0aaaeb10bf4d919a",
+    "url": "/cv_creator/static/js/main.e36c0aaa.chunk.js"
+  },
+  {
+    "revision": "1b4faa89122c1d7c0124",
+    "url": "/cv_creator/static/js/runtime~main.1b4faa89.js"
+  },
+  {
+    "revision": "30c60ac21feff76f247a",
+    "url": "/cv_creator/static/js/2.30c60ac2.chunk.js"
+  },
+  {
+    "revision": "d03c5b407f7fd1381a4761b8907a7d68",
+    "url": "/cv_creator/static/media/min.d03c5b40.jpg"
+  },
+  {
+    "revision": "04101f5961da0d14f96edff91b8e5f73",
+    "url": "/cv_creator/static/media/class.04101f59.jpg"
+  },
+  {
+    "revision": "5677d7987b89e46c80bf27e765bcd4b4",
+    "url": "/cv_creator/static/media/sides.5677d798.jpg"
+  },
+  {
+    "revision": "71636df96d83c91584410de629d50cee",
+    "url": "/cv_creator/index.html"
+  }
+];
